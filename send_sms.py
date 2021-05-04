810897e5efffc611703d9ef7fc662af78691192d
AMAL MATHEW <67771115+amalmathewcodes@users.noreply.github.com>
2021-05-04 07:02:30 +0530

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -6,7 +6,7 @@ from credentials import account_sid , auth_token ,my_cell ,my_twilio
 client =Client(account_sid , auth_token)
 
 
-my_msg = 'For the Google Ceo , You are the best in the world'
+my_msg = 'Your message goes here..'
 
 
 message = client.messages.create(to=my_cell,from_ = my_twilio , body = my_msg)
